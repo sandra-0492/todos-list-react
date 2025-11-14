7c7725731e52da032f023fb3ada308324c0da185
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-14 23:05:07 +0100

    formatted code

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,27 +1,27 @@
 import React from "react";
-import './style.css';
+import "./style.css";
 
 const Tasks = ({ tasks, hideDoneTasks }) => (
   <ul className="tasks">
-    {tasks.map(task => (
+    {tasks.map((task) => (
       <li
-      key={task.id}
+        key={task.id}
         className={`tasks__item${
           task.done && hideDoneTasks ? " tasks__item--hidden" : ""
         }`}
       >
-        <button className="tasks__button tasks__button--toggleDone">
+        <button 
+        className="tasks__button tasks__button--toggleDone">
           {task.done ? "✓" : ""}
         </button>
         <span
           className={`tasks__content${
-            task.done ? " tasks__content--done" : ""}`
-          }>
+            task.done ? " tasks__content--done" : ""
+          }`}
+        >
           {task.content}
         </span>
-        <button className="tasks__button tasks__button--remove">
-          🗑️
-        </button>
+        <button className="tasks__button tasks__button--remove">🗑️</button>
       </li>
     ))}
   </ul>
