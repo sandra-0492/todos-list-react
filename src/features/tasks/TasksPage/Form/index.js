a986ae3ff74763a9a75e4adcc82f43456a10c397
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-28 13:27:12 +0100

    adding correct formatting

---
+++ src/features/tasks/TasksPage/Form/index.js
@@ -0,0 +1,45 @@
+import { useState, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { Button, StyledForm } from "./styled";
+import { addTask } from "../../tasksSlice";
+import Input from "../../Input";
+
+const Form = () => {
+  const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
+
+  const dispatch = useDispatch();
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const trimmedNewTaskContent = newTaskContent.trim();
+    if (!trimmedNewTaskContent) {
+      return;
+    }
+
+    dispatch(addTask({
+        content: trimmedNewTaskContent,
+        done: false,
+        id: nanoid(),
+      }));
+
+    setNewTaskContent("");
+    inputRef.current.focus();
+  };
+
+  return (
+    <StyledForm onSubmit={onFormSubmit}>
+      <Input
+        ref={inputRef}
+        value={newTaskContent}
+        placeholder="Co jest do zrobienia?"
+        onChange={({ target }) => setNewTaskContent(target.value)}
+      />
+      <Button>Dodaj zadanie</Button>
+    </StyledForm>
+  );
+};
+
+export default Form;
