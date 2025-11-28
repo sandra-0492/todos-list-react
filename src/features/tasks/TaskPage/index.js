6377eca8dcd4f3e0466bb5579ef6af28276f0d23
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-28 14:53:22 +0100

    added correct formatting

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -7,7 +7,7 @@ import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
   const { id } = useParams();
-  const task = useSelector((state) => getTaskById(state, id));
+  const task = useSelector(state => getTaskById(state, id));
 
   return (
     <Container>
