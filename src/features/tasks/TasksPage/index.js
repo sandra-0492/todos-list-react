7faff0881144f891626715d14e0d9e04c92a236c
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-28 13:22:02 +0100

    created TasksPage index.js

---
+++ src/features/tasks/TasksPage/index.js
@@ -0,0 +1,30 @@
+import Form from "./Form";
+import TaskList from "./TaskList";
+import Buttons from "./Buttons";
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import Container from "../../../common/Container";
+import Search from "./Search";
+
+function TasksPage() {
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section 
+      title="Dodaj nowe zadanie" 
+      body={<Form />} 
+      />
+      <Section 
+      title="Wyszukiwarka" 
+      body={<Search />} 
+      />
+      <Section
+        title="Lista zadań"
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
+      />
+    </Container>
+  );
+}
+
+export default TasksPage;
