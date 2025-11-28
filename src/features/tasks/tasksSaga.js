68292a5d6798b60fad9b7b28b9211de938e52642
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-28 13:19:33 +0100

    created tasksSaga

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,37 @@
+import {
+  takeLatest,
+  call,
+  put,
+  delay,
+  select,
+  takeEvery,
+} from "redux-saga/effects";
+import {
+  fetchExampleTasks,
+  selectTasks,
+  fetchExampleTasksSuccess,
+  fetchExampleTasksError,
+} from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+  try {
+    yield delay(1000);
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(fetchExampleTasksSuccess(exampleTasks));
+  } catch (error) {
+    yield put(fetchExampleTasksError());
+    yield call(alert, "Coś tu nie gra 🤔");
+  }
+}
+
+function* saveTasksInLocalStorageHandler() {
+  const tasks = yield select(selectTasks);
+  yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+  yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+  yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
