137a28136a3c66fb78829a8c5cced2490442a06b
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-28 13:21:46 +0100

    created TaskPage index.js

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,27 @@
+import Section from "../../../common/Section";
+import Header from "../../../common/Header";
+import Container from "../../../common/Container";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
+
+function TaskPage() {
+  const { id } = useParams();
+  const task = useSelector((state) => getTaskById(state, id));
+
+  return (
+    <Container>
+      <Header title="Szczegóły zadania" />
+      <Section
+        title={task ? task.content : "Oops! Nie znaleziono zadania 😒"}
+        body={!!task && (
+          <>
+            <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
+          </>
+        )}
+      />
+    </Container>
+  );
+}
+
+export default TaskPage;
