1ba8005b0727431823b55f62a217640284327657
Sandra Masternak <sandra.masternak1@gmail.com>
2025-11-28 13:17:02 +0100

    added import HashRouter, Switch, Route, Redirect, Tasks, Task, Author, oAuthor, toTask, toTasks, Navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import Tasks from "./features/tasks/TasksPage";
+import Task from "./features/tasks/TaskPage";
+import Author from "./features/author/AuthorPage"
+import { toAuthor, toTask, toTasks } from "./routes";
+import Navigation from "./Navigation";
 
-const App = () => <Tasks />;
+export default () => (
+  <HashRouter>
+    <Navigation />
+      
+      <Switch>
+        <Route path={toTask()}>
+          <Task />
+        </Route>
+        <Route path={toTasks()}>
+          <Tasks />
+        </Route>
+        <Route path={toAuthor()}>
+          <Author />
+        </Route>
+        <Route>
+          <Redirect to={toTasks()} />
+        </Route>
+      </Switch>
+  </HashRouter>
+);
 
-export default App;
